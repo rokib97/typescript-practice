3a8566d367e37fa7e9b65fde2ba181e0214bcefb
rokib97 <hasanrokib1997@gmail.com>
2022-08-21 08:10:26 +0600

    explicit and any type

--- output/script.js
+++ output/script.js
@@ -27,3 +27,60 @@ const person = {
 person.name = "Rokibul";
 person.age = 25;
 // person.Newage = 25; //not valid
+// Explicitly declare
+let a;
+a = "rokib";
+let b;
+b = 20;
+let c;
+c = false;
+// array
+let numbers = [];
+let names = [];
+names.push("rokib");
+numbers.push(10);
+let mixed = [];
+mixed.push(10);
+mixed.push("hello");
+console.log(numbers, names, mixed);
+// for variable
+let myVar;
+myVar = 10;
+myVar = "Rokib";
+console.log(myVar);
+// object
+let Myobj;
+Myobj = {
+    name: "rokib",
+    age: 21,
+    isFunny: true,
+};
+console.log(Myobj);
+let MyObj2 = {
+    name: "Rokib",
+    age: 25,
+};
+console.log(MyObj2);
+let myObj3;
+myObj3 = {
+    name: "rokib",
+    age: 21,
+    isSmart: true,
+};
+console.log(myObj3);
+// array is an object too
+let arrObj;
+arrObj = [10, 20, 30];
+// Dynamic / Any type
+let first;
+first = 10;
+first = "Hello";
+let myArrOfAnny = [];
+myArrOfAnny.push("hello");
+myArrOfAnny.push(96);
+myArrOfAnny.push(false);
+let firstObject;
+firstObject = {
+    name: "Rakib",
+    age: 22,
+};
--- src/script.ts
+++ src/script.ts
@@ -31,3 +31,84 @@ const person = {
 person.name = "Rokibul";
 person.age = 25;
 // person.Newage = 25; //not valid
+
+// Explicitly declare
+let a: string;
+a = "rokib";
+
+let b: number;
+b = 20;
+
+let c: boolean;
+c = false;
+
+// array
+let numbers: number[] = [];
+let names: string[] = [];
+names.push("rokib");
+numbers.push(10);
+
+let mixed: (number | string)[] = [];
+mixed.push(10);
+mixed.push("hello");
+
+console.log(numbers, names, mixed);
+
+// for variable
+let myVar: number | string;
+myVar = 10;
+myVar = "Rokib";
+console.log(myVar);
+
+// object
+
+let Myobj: object;
+Myobj = {
+  name: "rokib",
+  age: 21,
+  isFunny: true,
+};
+console.log(Myobj);
+
+let MyObj2: object = {
+  name: "Rokib",
+  age: 25,
+};
+console.log(MyObj2);
+
+let myObj3: {
+  name: string;
+  age: number;
+  isSmart: boolean;
+};
+myObj3 = {
+  name: "rokib",
+  age: 21,
+  isSmart: true,
+};
+console.log(myObj3);
+
+// array is an object too
+
+let arrObj: object;
+arrObj = [10, 20, 30];
+
+// Dynamic / Any type
+
+let first: any;
+first = 10;
+first = "Hello";
+
+let myArrOfAnny: any[] = [];
+myArrOfAnny.push("hello");
+myArrOfAnny.push(96);
+myArrOfAnny.push(false);
+
+let firstObject: {
+  name: any;
+  age: any;
+};
+firstObject = {
+  name: "Rakib",
+  age: 22,
+};
